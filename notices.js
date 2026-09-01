50ff6c2153456db23d121b91daf4df42a7930120
Francisco Albuquerque <anestesiahmtforms@gmail.com>
2026-09-01 17:48:48 -0300

    Update notices.js

--- notices.js
+++ notices.js
@@ -6,8 +6,8 @@ window.SAHMT_NOTICES = {
     {
       id: "aviso-principal",
       eyebrow: "Comunicado SAHMT",
-      title: "https://drive.google.com/file/d/1TWuarxdC3qDIZYRtZQ3xExCoKz4oTTuL/view?usp=drive_link",
-      message: .
+      title: "TRINAMENTO APP",
+      message: https://drive.google.com/file/d/1TWuarxdC3qDIZYRtZQ3xExCoKz4oTTuL/view?usp=drive_link.
 
 
 
